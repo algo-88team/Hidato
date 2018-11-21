c5f53dd7fed060a36bb4e3e08619ccb9c1209855
thd0222 <thdtjddb@naver.com>
2018-11-21 21:01:22 +0900

    Build main prototype

--- main.cpp
+++ main.cpp
@@ -2,7 +2,24 @@
 // Created by thdtj on 2018-11-21.
 //
 
+#include <iostream>
+
+#include "Puzzle.h"
+#include "Generator.h"
+#include "Solver.h"
+
 int main() {
+    Puzzle puzzle;
+    Generator generator;
+    Solver solver;
+
+    puzzle = generator.LoadFormFile();
+
+    std::cout << "Generated puzzle" << std::endl;
+    std::cout << puzzle << std::endl << std::endl;
+
+    std::cout << "Solved puzzle" << std::endl;
+    std::cout << solver.solve(puzzle) << std::endl;
 
     return 0;
 }
