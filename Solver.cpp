ef8cca91f4a12098b6d1044a9b0f5b2baa3708cb
eunyyyoung <any218@hanmail.net>
2018-12-16 22:56:30 +0900

    빈칸 -1, 벽 0 통일

--- Solver.cpp
+++ Solver.cpp
@@ -31,7 +31,7 @@ bool Stack::empty() {
 }
 
 int Solver::hidato_solve(int num, const int goal, int position_x, int position_y, Puzzle& p) {
-	/***************************** Recursive *********************************************/
+	//***************************** Recursive *********************************************
 	if (num == goal)
 		return 1;
 
@@ -41,77 +41,78 @@ int Solver::hidato_solve(int num, const int goal, int position_x, int position_y
 		if (next_x >= 0 && next_x < p.getWidth() && next_y >= 0 && next_y < p.getHeight() && p[next_y][next_x] == num + 1) {
 			if (hidato_solve(num + 1, goal, next_x, next_y, p))
 				return 1;
-			}
+		}
 	}
 
 	for (int i = 0; i < 8; i++) {
 		int next_x = position_x + this->direction[i][0];
 		int next_y = position_y + this->direction[i][1];
 
-		if (next_x >= 0 && next_x < p.getWidth() && next_y >= 0 && next_y < p.getHeight() && p[next_y][next_x] == 0) {
-			
+		if (next_x >= 0 && next_x < p.getWidth() && next_y >= 0 && next_y < p.getHeight() && p[next_y][next_x] == -1) {
+
 			p[next_y][next_x] = num + 1;
 
 			if (hidato_solve(num + 1, goal, next_x, next_y, p))
 				return 1;
 
-			p[next_y][next_x] = 0;	
+			p[next_y][next_x] = -1;
 		}
 	}
-	
+	return 0;
+
 	/********************************** Stack **********************************************
 	Puzzle temp(p);
-	// std::stack<int> s;
 	Stack s;
-	s.push(position_x, position_y,0);
+	s.push(position_x, position_y, 0);
 	int found = 0;
 	while (!s.empty() && !found) {
-		int dir = s.pop();
-		int y = s.pop();
-		int x = s.pop();
+	int dir = s.pop();
+	int y = s.pop();
+	int x = s.pop();
 
-		while (dir < 8 && !found) {
-			int next_x = x + this->direction[dir][0];
-			int next_y = y + this->direction[dir][1];
+	while (dir < 8 && !found) {
+	int next_x = x + this->direction[dir][0];
+	int next_y = y + this->direction[dir][1];
 
-			if (num == goal)
-				found = 1;
+	if (num == goal)
+	found = 1;
 
-			else if (next_x >= 0 && next_x < p.getWidth() && 
-					 next_y >= 0 && next_y < p.getHeight() && 
-					 (p[next_y][next_x] == num + 1 || p[next_y][next_x] == 0)) {
-				if (p[next_y][next_x] == num + 1) {
-					s.push(x, y, ++dir);
-					x = next_x; y = next_y; dir = 0;
-					++num;
-				}
-				else {
-					p[next_y][next_x] = ++num;
-					s.push(x, y, ++dir); 
-					x = next_x; y = next_y; dir = 0;
-				}
-			}
-			else
-				dir++;
-		}
-		p[y][x] = temp[y][x];
-		num--;
+	else if (next_x >= 0 && next_x < p.getWidth() &&
+	next_y >= 0 && next_y < p.getHeight() &&
+	(p[next_y][next_x] == num + 1 || p[next_y][next_x] == -1)) {
+	if (p[next_y][next_x] == num + 1) { //다음 숫자 적혀있으면
+	s.push(x, y, ++dir);
+	x = next_x; y = next_y; dir = 0;
+	++num;
+	}
+	else {   //빈칸이면
+	p[next_y][next_x] = ++num;
+	s.push(x, y, ++dir);
+	x = next_x; y = next_y; dir = 0;
+	}
+	}
+	else
+	dir++;
+	}
+	p[y][x] = temp[y][x];
+	num--;
 
 	}
-	*/
+
 	return 0;
+	*/
 }
 
 Puzzle &Solver::Solve(Puzzle &p) {
-    // TODO
-	int goal = 0;
+	// TODO
+	int goal = -1;
 	int position_x = 0, position_y = 0;
-	
+
 	// find goal, find start_point
 	for (int i = 0; i < p.getHeight(); i++) {
 		for (int j = 0; j < p.getWidth(); j++) {
 			int temp = p[i][j];
-			if (temp != -1) {
+			if (temp != 0) {
 				if (temp > goal)
 					goal = temp;
 				if (temp == 1) {
@@ -124,7 +125,13 @@ Puzzle &Solver::Solve(Puzzle &p) {
 
 	//std::cout << goal << std::endl << position_x << " " << position_y << std::endl;
 	// solve_program
+
+	clock_t begin = clock();
 	hidato_solve(1, goal, position_x, position_y, p);
-	
-    return p;
+	clock_t end = clock();
+	long msec1 = end - begin;
+	std::cout << msec1 * 0.001 << " 초" << "\n";
+
+
+	return p;
 }
